92f9ea607928d6f0cd5754eee8abb4014c8fdd20
Nontaphat003 <nontaphat.b@gmail.com>
2024-01-16 08:39:04 +0700

    done

---
+++ Database/MinTam/Ex1.js
@@ -0,0 +1,14 @@
+// หาหาร 2
+// numberSplit(4) ➞ [2, 2]
+// numberSplit(10) ➞ [5, 5]
+// numberSplit(11) ➞ [5, 6]
+// numberSplit(-9) ➞ [-5, -4]
+
+function numberSplit(n) {
+    return [Math.floor(n / 2), Math.ceil(n / 2)];    // 1คะแนน
+}
+
+console.log(numberSplit(4));
+console.log(numberSplit(10));
+console.log(numberSplit(11));
+console.log(numberSplit(-9));
---
+++ Database/MinTam/Ex2.js
@@ -0,0 +1,20 @@
+// highestdigit(379) -> 9
+// highestdigit(2) -> 2
+// highestdigit(377401) -> 7
+
+function highestDigit(n) {
+    let max = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        if (digit > max) {
+            max = digit;
+
+        }
+        n = Math.floor(n / 10);
+    }
+    return max;
+}
+
+console.log(highestDigit(379));
+console.log(highestDigit(2));
+console.log(highestDigit(377401));
