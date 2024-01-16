8f60234d5d35022315ff3ac7a31b65902d4fe829
Nontaphat003 <nontaphat.b@gmail.com>
2024-01-16 10:38:38 +0700

    done

--- MinTam/Ex10.js
+++ MinTam/Ex10.js
@@ -1,3 +1,4 @@
+// หาเลข IPv (เลขไม่เกิน 255)
 // isValidIP("1.2.3.4") -> true
 // isValidIP("1.2.3") -> false
 // isValidIP("1.2.3.4.5") -> false
@@ -6,9 +7,21 @@
 // isValidIP("123.045.067.089") -> false
 
 function isValidIP(str) {
-    
+    let arr = str.split(".");
+    if (arr.length != 4) {
+        return false;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(arr[i]);
+        if (arr[i] < 0 || arr[i] > 255) {
+            return false;
+        }
+        if (arr[i][0] == 0 && arr[i].length > 1) {
+            return false;
+        }
+    }
+    return true;
 }
-
 
 console.log(isValidIP("1.2.3.4"));
 console.log(isValidIP("1.2.3"));
