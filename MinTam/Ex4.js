ea212550a751c4d7825a11267bf685d779772421
Nontaphat003 <nontaphat.b@gmail.com>
2024-01-16 09:34:22 +0700

    done

---
+++ MinTam/Ex4.js
@@ -0,0 +1,16 @@
+// คูณไปเรื่อยๆ
+// totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]) ➞ 63
+// totalVolume([2, 2, 2], [2, 1, 1]) ➞ 10
+// totalVolume([1, 1, 1]) ➞ 1
+
+function totalVolume(...args) {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i][0] * args[i][1] * args[i][2];
+    }
+    return total;
+}
+
+console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]));
+console.log(totalVolume([2, 2, 2], [2, 1, 1]));
+console.log(totalVolume([1, 1, 1]));
