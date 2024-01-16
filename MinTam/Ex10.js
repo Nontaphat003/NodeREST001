30cdcf4cf03644fa7ddee048a34b4506dcc21eee
Nontaphat003 <nontaphat.b@gmail.com>
2024-01-16 10:29:24 +0700

    done

---
+++ MinTam/Ex10.js
@@ -0,0 +1,18 @@
+// isValidIP("1.2.3.4") -> true
+// isValidIP("1.2.3") -> false
+// isValidIP("1.2.3.4.5") -> false
+// isValidIP("123.45.67.89") -> true
+// isValidIP("123.456.78.90") -> false
+// isValidIP("123.045.067.089") -> false
+
+function isValidIP(str) {
+    
+}
+
+
+console.log(isValidIP("1.2.3.4"));
+console.log(isValidIP("1.2.3"));
+console.log(isValidIP("1.2.3.4.5"));
+console.log(isValidIP("123.45.67.89"));
+console.log(isValidIP("123.456.78.90"));
+console.log(isValidIP("123.045.067.089 "));
