96076505e4820264bc0cb6efbb1ad38737cdbb0e
Your Name Nontaphat003 <s6506021620083@email.kmutnb.ac.th>
2024-02-06 09:05:37 +0700

    6/2/2024

--- index.js
+++ index.js
@@ -69,7 +69,115 @@ app.post('book', (req, res) => {
             res.status(500).send(err);
         } else {
             book.id = this.lastID;
-            res.send(book);
+            res.send(book);//
+            //
+            //
+            //
+            
+            const express = require('express');
+            const Sequelize = require('sequelize');
+            const qpp = express();
+            
+            // parse incoming requests
+            application.use(express.json());
+            
+            // create a connection to the database
+            const sequelize = new Sequelize('database','username','password',{
+                host: 'localhost',
+                dialect: 'sqlite',
+                storage: './Database/SQBooks.sqlite'
+            });
+            
+            // define the Book model
+            const Book = sequelize.define('book',{
+                id: {
+                    type: Sequelize.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true
+                },
+                title: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                author: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                }
+            });
+            
+            // create the books table if it doesn't exist
+            sequelize.sync();
+            
+            // route to get all books
+            app.get('/book', (req, res) => {
+                Book.findAll().then(books => {
+                    res.json(books);
+                }).catch(err => {
+                    res.status(500).send(err);
+                });
+            });
+            
+            // route to get a book by id
+            app.get('/book/:id', (req, res) => {
+                Book.findByPk(req.params.id).then(book => {
+                    if (!book) {
+                        res.status(404).send('Book not found');
+                    } else {
+                        res.json(book);
+                    }
+                }).catch(err => {
+                    res.status(500).send(err);
+                });
+            });
+            
+            // route to create a new book
+            app.post('/books', (req, res) => {
+                Book.create(req.body).then(book => {
+                    res.send(book);
+                }).catch(err => {
+                    res.status(500).send(err);
+                });
+            });
+            
+            // route to update a book
+            app.put('/book/:id', (req,res) => {
+                Book.findByPk(req.params.id).then(book => {
+                    if (!book) {
+                        res.status(404).send('Book not found');
+                    } else {
+                        book.update(req.body).then(() => {
+                            res.send(book);
+                        }).catch(err => {
+                            res.status(500).send(err);
+                        });
+                    }
+                }).catch(err => {
+                    res.status(500).send(err);
+                });
+            });
+            
+            // route to delete a book
+            app.delete('/book/:id', (req,res) => {
+                Book.findByPk(req.params.id).then(book => {
+                    if (!book) {
+                        res.status(404).send('Book not found');
+                    } else {
+                        book.destroy().then(() => {
+                            res.send({});
+                        }).catch(err => {
+                            res.send(500).send(err);
+                        });
+                    }
+                }).catch(err => {
+                    res.status(500).send(err);
+                });
+            });
+            
+            // start the sever
+            const port = process.env.PORT || 3000;
+            app.listen(port, () => console.log(`Listening on port ${port}...`));
+            
+            
         }
     });
 });
