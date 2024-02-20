cfcb04eaf7ebe714596265a0e6883b4ea9b06493
Your Name Nontaphat003 <s6506021620083@email.kmutnb.ac.th>
2024-02-20 10:11:00 +0700

    8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json())
 
-const dbUrl = 'postgres://webadmin:AQGrys74691@node58272-noderest0.proen.app.ruk-com.cloud:/Books'
+const dbUrl = 'postgres://webadmin:AQGrys74691@node58272-noderest0.proen.app.ruk-com.cloud/Books'
 
 const sequelize = new Sequelize(dbUrl)
 
