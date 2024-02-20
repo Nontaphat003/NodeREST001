edf4a15b2c1141fe7916ec1b70ed4951759e336a
Your Name Nontaphat003 <s6506021620083@email.kmutnb.ac.th>
2024-02-20 11:11:02 +0700

    8

--- SRC/PGSqlCRUD.js
+++ SRC/PGSqlCRUD.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.json())
 
-const dbUrl = 'postgres://webadmin:AQGrys74691@node58272-noderest0.proen.app.ruk-com.cloud:11764/Books'
+const dbUrl = 'postgres://webadmin:AQGrys74691@node58272-noderest0.proen.app.ruk-com.cloud/Books'
 
 const sequelize = new Sequelize(dbUrl)
